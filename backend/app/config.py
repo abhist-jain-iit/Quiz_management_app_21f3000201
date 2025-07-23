1844d8c0e15be4e53766e9fce08c4a2f23a15a65
abhist <abhistjain@gmail.com>
2025-07-23 20:11:41 +0530

    Comment

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,6 +5,7 @@ BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
     SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT', 'dev-salt')
+    # Used to add an extra layer of security when generating tokens for things
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_PATH, 'quiz_master.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
